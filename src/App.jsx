2948bd36bc54f1e3939edf7b7ba0e7bc553be1aa
Abir <abirovi481@gmail.com>
2023-03-31 00:34:47 +0600

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/collapse'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Question from './components/Question/Question';
 
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
-       <Main></Main>
+        <Main></Main>
+        <Question></Question>
     </div>
 
   )
--- src/components/Side-bar/Side.jsx
+++ src/components/Side-bar/Side.jsx
@@ -7,13 +7,13 @@ const Side = (props) => {
         for(const card of time){
             totalTime = totalTime + card.time
         }
-        let title = ''
+        let title = '';
         for(const card of bookMark){
             title = card.title
         }
     return (
         <div className='position-sticky top-0'>
-            <h5 style={{background: "rgba(96, 71, 236, 0.2)"}} className='w-100 border border-primary p-3 rounded mb-4'>Spent time on read : {totalTime}</h5>
+            <h5 style={{background: "rgba(96, 71, 236, 0.2)"}} className='w-100 border border-primary p-3 rounded mb-4'>Spent time on read : {totalTime} Min</h5>
             <div>
                 <h5 className='mb-3'>Bookmarked Blogs : {bookMark.length}</h5>
                 <h3 className='mb-3'>{title}</h3>
