d494c5c1609f95a881f855dc99296e47aa150dad
Abir <abirovi481@gmail.com>
2023-03-31 21:39:45 +0600

    eight-commit

--- src/components/Side-bar/Side.jsx
+++ src/components/Side-bar/Side.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const Side = (props) => {
         const {time} = props;
@@ -6,17 +6,18 @@ const Side = (props) => {
         let totalTime = 0;
         for(const card of time){
             totalTime = totalTime + card.time
-        }
-        let title = '';
-        for(const card of bookMark){
-            title = card.title
+            console.log(card)
         }
     return (
         <div className='position-sticky top-0'>
             <h5 style={{background: "rgba(96, 71, 236, 0.2)"}} className='w-100 border border-primary p-3 rounded mb-4'>Spent time on read : {totalTime} Min</h5>
-            <div>
+            <div className="p-3 rounded" style={{background:"pink"}}>
                 <h5 className='mb-3'>Bookmarked Blogs : {bookMark.length}</h5>
-                <h3 className='mb-3'>{title}</h3>
+                <div>
+                    {
+                      bookMark.map(card => <h3 className="rounded p-3 shadow-lg" style={{background:"whitesmoke"}}>{card.title}</h3>)
+                    }
+                </div>
             </div>
         </div>
     );
